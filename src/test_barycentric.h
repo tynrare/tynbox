7087799541af36babbf7b89dadbccfd598c038e7
tynroar <tynrare@gmail.com>
2024-06-25 04:29:14 +0300

    test switch added

---
+++ src/test_barycentric.h
@@ -0,0 +1,9 @@
+#include <raylib.h>
+#include <raymath.h>
+
+Vector3 get_barecentric_coordinates(Vector3 v0, Vector3 v1, Vector3 v2,
+                                    Vector3 p);
+
+Vector3 barycentric_to_cartesian(Vector3 bc_pos, Vector3 v0, Vector3 v1,
+                                 Vector3 v2);
+void _draw_test_barycentric();
